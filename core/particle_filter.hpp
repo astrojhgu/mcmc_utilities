59433238846144c86f27de7af4e616e8ec73a2a0
astrojhgu <astrojhgu@gmail.com>
2015-12-20 00:30:06 +0800

    修改：     core/particle_filter.hpp 	更改particle filter的proposal distribution

--- core/particle_filter.hpp
+++ core/particle_filter.hpp
@@ -39,12 +39,24 @@ namespace mcmc_utilities
     typedef typename element_type_trait<T_state>::element_type T_state1;
   private:
     pf_model(const pf_model&);
+    T_p alpha;
+    
   public:
     typedef typename element_type_trait<T_state>::element_type stat_element_type;
     typedef typename element_type_trait<T_obs>::element_type obs_element_type;
+    
+    
   public:
-    pf_model(){}
+    pf_model()
+      :alpha(.5)
+    {}
     virtual ~pf_model(){}
+
+    void set_proposed_distributin_factor(const T_p& p)
+    {
+      alpha=p;
+    }
+    
     T_p evol_log_prob(const T_state& x,const T_t& t,const T_state& prev_state,const T_t& prev_t)const
     {
       return do_evol_log_prob(x,t,prev_state,prev_t);
@@ -58,6 +70,11 @@ namespace mcmc_utilities
     T_p combined_log_prob(const T_obs& y,const T_state& x,const T_t& t,const T_state& prev_state,const T_t& prev_t)const
     {
       return evol_log_prob(x,t,prev_state,prev_t)+obs_log_prob(y,x,t);
+    }
+
+    T_p proposed_log_prob(const T_obs& y,const T_state& x,const T_t& t,const T_state& prev_state,const T_t& prev_t)const
+    {
+      return alpha*combined_log_prob(y,x,t,prev_state,prev_t);
     }
     
     std::pair<T_state1,T_state1> state_var_range(const T_t& t,const T_state& prev_state,const T_t& prev_t,size_t ndim)const
@@ -89,7 +106,9 @@ namespace mcmc_utilities
       public:
 	T_p do_eval_log(const T_state& x)const
 	{
-	  return ptr_pf_model->evol_log_prob(x,*ptr_t,*ptr_particle,*ptr_prev_t);
+	  //return ptr_pf_model->evol_log_prob(x,*ptr_t,*ptr_particle,*ptr_prev_t);
+	  //return ptr_pf_model->evol_log_prob(x,*ptr_t,*ptr_particle,*ptr_prev_t);
+	  return ptr_pf_model->proposed_log_prob(*ptr_obs_vec,x,*ptr_t,*ptr_particle,*ptr_prev_t);
 	}
 	std::pair<T_var1,T_var1> do_var_range(const T_state& x,size_t ndim)const
 	{
@@ -108,7 +127,7 @@ namespace mcmc_utilities
       std::vector<T_p> weight_cdf(particle_list.size());
       std::vector<particle<T_p,T_state> > updated_state(particle_list.size());
       std::vector<T_p> log_weight(particle_list.size());
-#pragma omp parallel for
+      
       for(size_t i=0;i<particle_list.size();++i)
 	{
 	  cprob prob;
@@ -122,10 +141,12 @@ namespace mcmc_utilities
 
 	  gibbs_sample(prob,new_pred,rnd);
 	  
-	  particle_list[i].state=new_pred;
+	  
 	  //particle_list[i].weight=std::exp(obs_log_prob(y,new_pred,t));
-	  log_weight[i]=obs_log_prob(y,new_pred,t);
+	  //log_weight[i]=obs_log_prob(y,new_pred,t);
+	  log_weight[i]=(1-alpha)*combined_log_prob(y,new_pred,t,particle_list[i].state,prev_t);
 	  //cout<<new_pred[0]<<" "<<particle_list[i].weight<<endl;
+	  particle_list[i].state=new_pred;
 	}
       T_p max_log_weight=*(std::max_element(log_weight.begin(),log_weight.end()));
 #pragma omp parallel for
