0f374b56c56124d6374dbef83fc460c4e2b4fb77
astrojhgu <astrojhgu@gmail.com>
2015-12-21 19:12:27 +0800

    p 	新文件：   rng/normal_rng.hpp

---
+++ rng/normal_rng.hpp
@@ -0,0 +1,26 @@
+#ifndef NORMAL_RNG_HPP
+#define NORMAL_RNG_HPP
+
+#include <random>
+#include <cmath>
+#include "../core/base_urand.hpp"
+
+namespace mcmc_utilities
+{
+  template <typename T>
+  T normal_rng(const base_urand<T>& urand)
+  {
+    static std::normal_distribution<T> dist((T)0,(T)1);
+    return dist(urand);
+  }
+
+  template <typename T>
+  T normal_rng(T m,T s,const base_urand<T>& urand)
+  {
+    return normal_rng(urand)*s+m;
+  }
+};
+
+
+
+#endif
